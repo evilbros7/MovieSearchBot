e8d2f8a9a6362d4694e2e7352ca63ab8107d83de
evilbros7 <69081291+evilbros7@users.noreply.github.com>
2021-11-28 00:21:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -66,8 +66,8 @@ async def start(bot, cmd):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('Search again', switch_inline_query_current_chat=''),
-                        InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
+                        InlineKeyboardButton('🔍Search Again🔎', switch_inline_query_current_chat=''),
+                        InlineKeyboardButton('Join Now🎉 ', url='https://t.me/CoursesDrive')
                     ]
                     ]
                 await bot.send_cached_media(
@@ -100,7 +100,7 @@ async def start(bot, cmd):
                 [
                     [
                         InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
-                        InlineKeyboardButton("Other Bots", url="https://t.me/subin_works/122")
+                        InlineKeyboardButton("Join Now🎉", url="https://t.me/CoursesDrive")
                     ],
                     [
                         InlineKeyboardButton("About", callback_data="about")
@@ -192,8 +192,8 @@ async def delete(bot, message):
 async def bot_info(bot, message):
     buttons = [
         [
-            InlineKeyboardButton('Update Channel', url='https://t.me/subin_works'),
-            InlineKeyboardButton('Source Code', url='https://github.com/subinps/Media-Search-bot')
+            InlineKeyboardButton('Update Channel', url='https://t.me/CoursesDrive'),
+            InlineKeyboardButton('Source Code', url='https://t.me/CoursesDrive')
         ]
         ]
-    await message.reply(text="Language : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://github.com/subinps/Media-Search-bot'>Click here</a>\nUpdate Channel : <a href='https://t.me/subin_works'>XTZ Bots</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    await message.reply(text="Language : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a></a>\nUpdate Channel : <a href='https://t.me/CoursesDrive'>PREMIUM DRIVE</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
